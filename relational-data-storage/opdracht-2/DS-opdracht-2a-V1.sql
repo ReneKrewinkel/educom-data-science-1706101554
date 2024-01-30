d30cc545d3e5a5a578c72f6992b1a5c59f8ab407
Anne Goode <thegoodecode@gmail.com>
2024-01-30 12:38:11 +0100

    added analyserapport v1 opdracht 2

---
+++ relational-data-storage/opdracht-2/DS-opdracht-2a-V1.sql
@@ -0,0 +1,7 @@
+-- Analyseer de data kwaliteit
+-- Maak een rapport van je bevindingen
+-- Beschrijf de stappen die je moet zetten om de data integriteit op een acceptabel niveau te krijgen
+-- Hanteer de bekende Naming Conventions voor je rapport
+-- Maak een SQL script dat foreign key constraint die bij deze database horen verwijderd en vervolgens weer aanmaakt
+-- Noem de file: 'DS-opdracht-2a-VN.sql' waarbij N staat staat voor de versie van je scriptCommit en push je werk naar Github
+
