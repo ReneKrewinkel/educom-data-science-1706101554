997d06e9acd6e3b6789abab00da578d70a0e6686
Anne Goode <thegoodecode@gmail.com>
2024-02-06 11:17:36 +0100

    Opdracht 5.1.4

--- relational-data-storage/opdracht-5/DS-opdracht-5.sql
+++ relational-data-storage/opdracht-5/DS-opdracht-5.sql
@@ -33,10 +33,8 @@ MIN(mhl_hitcount.hitcount) AS min_hitcount, -- minimale hitcount
 MAX(mhl_hitcount.hitcount) AS max_hitcount, -- maximale hitcount
 AVG(mhl_hitcount.hitcount) AS avg_hitcount, -- gemiddelde hitcount
 SUM(mhl_hitcount.hitcount) AS sum_hitcount -- totale hitcount
-
 FROM
 mhl_hitcount
-
 GROUP BY year; -- distinct year
 
 -- 5.1.3 Selecteer uit de hitcount-tabel per jaar, per maand het aantal records, de minimale, maximale, gemiddelde en totale hitcount
@@ -49,8 +47,35 @@ MIN(mhl_hitcount.hitcount) AS min_hitcount,
 MAX(mhl_hitcount.hitcount) AS max_hitcount,
 AVG(mhl_hitcount.hitcount) AS avg_hitcount, 
 SUM(mhl_hitcount.hitcount) AS sum_hitcount
-
 FROM
 mhl_hitcount
-
 GROUP BY year, month;
+
+-- 5.1.4 Selecteer uit de hitcount-tabel de leveranciersnaam, het totaal aantal hits, het aantal maanden en de gemiddelde hitcount.
+
+
+SELECT
+mhl_suppliers.name,
+SUM(mhl_hitcount.hitcount) AS sum_hitcount,
+COUNT(mhl_hitcount.month) AS headcount_month,--DISTINCT Months? 12 =1 same as 5 = 1 as 1=1,
+AVG(mhl_hitcount.hitcount) AS avg_hitcount 
+FROM mhl_hitcount
+INNER JOIN 
+mhl_suppliers
+ON
+mhl_hitcount.supplier_ID = mhl_suppliers.ID;
+-- FAILED Had to use my comment as written at the COUNT + group the results by suppliername #1140 - In aggregated query without GROUP BY, expression #1 of SELECT list contains nonaggregated column 'mhl_opdrachten.mhl_suppliers.name'; this is incompatible with sql_mode=only_full_group_by
+
+
+SELECT
+mhl_suppliers.name,
+SUM(mhl_hitcount.hitcount) AS sum_hitcount,
+COUNT(DISTINCT mhl_hitcount.month) AS headcount_month,
+AVG(mhl_hitcount.hitcount) AS avg_hitcount 
+FROM mhl_hitcount
+INNER JOIN 
+mhl_suppliers
+ON
+mhl_hitcount.supplier_ID = mhl_suppliers.ID
+GROUP BY
+mhl_suppliers.name;
