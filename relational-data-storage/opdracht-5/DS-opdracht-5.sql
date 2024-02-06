8277d4622e621ba4fd5042507addda01b86a963f
Anne Goode <thegoodecode@gmail.com>
2024-02-06 16:27:04 +0100

    opdracht 5.2.1 still working on

--- relational-data-storage/opdracht-5/DS-opdracht-5.sql
+++ relational-data-storage/opdracht-5/DS-opdracht-5.sql
@@ -80,3 +80,78 @@ mhl_hitcount.supplier_ID = mhl_suppliers.ID
 GROUP BY
 mhl_suppliers.name;
 -- SUCCES (had to ROUND the AVG to match the example of the exercise)
+
+-- 5.2.1 Maak een verzendlijst voor alle directieleden van alle leveranciers, gesorteerd per provincie, plaatsnaam en naam: wanneer een postbusadres ingevuld dan dit gebruiken, anders vestigingsadres, wanneer contactpersoon aanwezig dan die naam in aanhef, anders 't.a.v. de directie'.
+
+-- Benodigde tabellen:
+-- Directieleden: mhl_departments: [ID, [name]] department_ID = 3 (directie)
+-- Leveranciers: mhl_suppliers: [ID, membertype, company, [name], straat, huisnr, postcode, city_ID, p_adress, p_postcode, p_city_ID]
+-- Provincie: mhl_communes: [ID, district_ID, [name]]
+-- Plaatsnaam: mhl_cities: [ID, commune_ID, [name]]
+-- Naam (leverancier): mhl_suppliers: [ID, membertype, company, [name], straat, huisnr, postcode, city_ID, p_adress, p_postcode, p_city_ID]
+-- Postadres:  mhl_suppliers: [ID, membertype, company, name, straat, huisnr, postcode, city_ID, [p_adress, p_postcode, p_city_ID]]
+-- Vestigingsadres:  mhl_suppliers: [ID, membertype, company, name, [straat, huisnr, postcode,] city_ID, p_adress, p_postcode, p_city_ID]
+-- Contactpersoon: mhl_contacts: [ID, supplier_ID, [department], contacttype, name, email, tel]
+
+SELECT
+mhl_suppliers.name AS leverancier,
+mhl_communes.name AS provincie,
+mhl_cities.name AS stad,
+
+---- Contactpersoon
+CASE 
+WHEN mhl_contacts.department = 3 
+THEN mhl_contacts.name 
+ELSE 'tav de directie' 
+END AS aanhef,
+
+---- Adres
+CASE
+WHEN mhl_suppliers.p_address IS NOT NULL
+THEN mhl_suppliers.p_address
+ELSE mhl_suppliers.straat
+END AS adres,
+
+---- Postcode
+CASE
+WHEN mhl_suppliers.p_postcode IS NOT NULL
+THEN mhl_suppliers.p_postcode
+WHEN mhl_suppliers.p_postcode IS NULL
+THEN mhl_suppliers.postcode
+END AS postcode
+
+-- ---- Stad
+-- SELECT
+-- CASE
+-- WHEN mhl_suppliers.p_city_ID IS NOT NULL 
+-- THEN mhl_suppliers.p_city_ID
+-- ELSE mhl_suppliers.city_ID
+-- END AS City_ID
+-- FROM mhl_suppliers
+
+JOIN
+mhl_cities
+ON 
+mhl_cities.ID = mhl_suppliers.city_ID
+
+JOIN 
+mhl_contacts
+ON 
+mhl_contacts.supplier_ID = mhl_suppliers.ID
+
+JOIN
+mhl_communes
+ON mhl_communes.ID = mhl_cities.commune_ID
+
+JOIN
+mhl_districts
+ON 
+mhl_districts.ID = mhl_communes.district_ID
+
+ORDER BY
+provincie,
+stad,
+leverancier;
+
+
+
