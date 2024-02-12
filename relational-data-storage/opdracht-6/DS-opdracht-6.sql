e69423e6668c4c6eaa3c18bb5fc43017d01b3f5b
Anne Goode <thegoodecode@gmail.com>
2024-02-12 11:21:51 +0100

    Opdracht 6.1.1

--- relational-data-storage/opdracht-6/DS-opdracht-6.sql
+++ relational-data-storage/opdracht-6/DS-opdracht-6.sql
@@ -4,7 +4,34 @@ mhl_hitcount: [supplier_ID, year, month, hitcount]
 
 SELECT
 mhl_hitcount.year AS jaar,
-mhl_hitcount.month AS maand,
+
+CASE
+WHEN mhl_hitcount.month = '1'
+THEN 'januari'
+WHEN mhl_hitcount.month = '2'
+THEN 'februari'
+WHEN mhl_hitcount.month = '3'
+THEN 'maart'
+WHEN mhl_hitcount.month = '4'
+THEN 'april'
+WHEN mhl_hitcount.month = '5'
+THEN 'mei'
+WHEN mhl_hitcount.month = '6'
+THEN 'juni'
+WHEN mhl_hitcount.month = '7'
+THEN 'juli'
+WHEN mhl_hitcount.month = '8'
+THEN 'augustus'
+WHEN mhl_hitcount.month = '9'
+THEN 'september'
+WHEN mhl_hitcount.month = '10'
+THEN 'oktober'
+WHEN mhl_hitcount.month = '11'
+THEN 'november'
+WHEN mhl_hitcount.month = '12'
+THEN 'december'
+END AS maand,
+
 COUNT(supplier_ID) AS aantal_leveranciers,
 SUM(mhl_hitcount.hitcount) AS totaal_hitcounts
 
@@ -13,4 +40,6 @@ mhl_hitcount
 
 GROUP BY year, month
 ORDER BY year, month
+
+
 
